f0bb4c192a35ca62b66b4ebdbaa18a4c208d69e5
Dmitry Shechtman <damnian@gmail.com>
2015-11-30 21:23:01 +0200

    Revert "Add MarkObsoleteAttribute"
    This reverts commit 2a3a665abd0a8258710f635c30d0151d985b76ee.

--- CommonMark/Syntax/Inline.cs
+++ CommonMark/Syntax/Inline.cs
@@ -143,13 +143,7 @@ namespace CommonMark.Syntax
         /// Gets or sets the first descendant of this element. This is only used if the <see cref="Tag"/> property specifies
         /// a type that can have nested elements. 
         /// </summary>
-        public Inline FirstChild
-        {
-            get;
-
-            [MarkObsolete("This property accessor is deprecated and will be removed in future versions. If you have a use case where this property accessor is required, please log an issue at https://github.com/Knagis/CommonMark.NET", false)]
-            set;
-        }
+        public Inline FirstChild { get; set; }
 
         /// <summary>
         /// Gets or sets the position of the element within the source data.
@@ -158,13 +152,7 @@ namespace CommonMark.Syntax
         /// in earlier parser stage, such as converting tabs to spaces).
         /// </summary>
         /// <seealso cref="SourceLength"/>
-        public int SourcePosition
-        {
-            get;
-
-            [MarkObsolete("This property accessor is deprecated and will be removed in future versions. If you have a use case where this property accessor is required, please log an issue at https://github.com/Knagis/CommonMark.NET", false)]
-            set;
-        }
+        public int SourcePosition { get; set; }
 
         internal int SourceLastPosition { get; set; }
 
@@ -176,22 +164,14 @@ namespace CommonMark.Syntax
         /// </summary>
         /// <seealso cref="SourcePosition"/>
         public int SourceLength 
-        {
-            get
-            {
-                return this.SourceLastPosition - this.SourcePosition;
-            }
-
-            [MarkObsolete("This property accessor is deprecated and will be removed in future versions. If you have a use case where this property accessor is required, please log an issue at https://github.com/Knagis/CommonMark.NET", false)]
-            set
-            {
-                this.SourceLastPosition = this.SourcePosition + value;
-            }
+        { 
+            get { return this.SourceLastPosition - this.SourcePosition; }
+            set { this.SourceLastPosition = this.SourcePosition + value; }
         }
 
         /// <summary>
         /// Gets the link details. This is now obsolete in favor of <see cref="TargetUrl"/> and <see cref="LiteralContent"/>
-        /// properties and this property will be removed in future versions.
+        /// properties and this property will be removed in future.
         /// </summary>
         [Obsolete("The link properties have been moved to TargetUrl and LiteralContent (previously Title) properties to reduce number of objects created. This property will be removed in future versions.", false)]
         [System.Diagnostics.DebuggerBrowsable(System.Diagnostics.DebuggerBrowsableState.Never)]
@@ -206,8 +186,6 @@ namespace CommonMark.Syntax
         public Inline NextSibling
         {
             get { return this._next; }
-
-            [MarkObsolete("This property accessor is deprecated and will be removed in future versions. If you have a use case where this property accessor is required, please log an issue at https://github.com/Knagis/CommonMark.NET", false)]
             set { this._next = value; }
         }
 
