0ab47686e1b74ff192eff89027b8dc80e1c042d0
Dmitry Shechtman <damnian@gmail.com>
2015-12-10 20:32:25 +0200

    Add TableCaptionData

---
+++ CommonMark/Syntax/TableCaptionData.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonMark.Syntax
+{
+    /// <summary>
+    /// Contains additional data for table caption element. Used in the <see cref="Block.TableCaptionData"/> property.
+    /// </summary>
+    public sealed class TableCaptionData
+    {
+        /// <summary>
+        /// Gets or sets the caption type.
+        /// </summary>
+        public TableCaptionType CaptionType { get; internal set; }
+    }
+}
