6e4202d76f3efd63a5547b1c9c5ba2c7073e052e
Dmitry Shechtman <damnian@gmail.com>
2015-12-13 09:28:57 +0200

    Move InlineDelimiterParameters to separate file

--- CommonMark/Parser/InlineParserParameters.cs
+++ CommonMark/Parser/InlineParserParameters.cs
@@ -1,51 +1,7 @@
-﻿using CommonMark.Syntax;
-using System;
+﻿using System;
 
 namespace CommonMark.Parser
 {
-    /// <summary>
-    /// Inline stack delimiter parameters.
-    /// </summary>
-    public struct InlineDelimiterParameters
-    {
-        /// <summary>
-        /// The tag to use for the inline element when the opener is matched.
-        /// </summary>
-        public InlineTag Tag;
-
-        /// <summary>
-        /// Determines whether the parameters instance is empty.
-        /// </summary>
-        public bool IsEmpty
-        {
-            get { return Tag == 0; }
-        }
-    }
-
-    /// <summary>
-    /// Inline stack delimiter character parameters.
-    /// </summary>
-    public struct InlineDelimiterCharacterParameters
-    {
-        /// <summary>
-        /// Determines whether the parameters instance is empty.
-        /// </summary>
-        public bool IsEmpty
-        {
-            get { return SingleCharacter.IsEmpty && DoubleCharacter.IsEmpty; }
-        }
-
-        /// <summary>
-        /// Single character delimiter parameters.
-        /// </summary>
-        public InlineDelimiterParameters SingleCharacter;
-
-        /// <summary>
-        /// Double character delimiter parameters.
-        /// </summary>
-        public InlineDelimiterParameters DoubleCharacter;
-    }
-
     /// <summary>
     /// Inline parser parameters.
     /// </summary>
