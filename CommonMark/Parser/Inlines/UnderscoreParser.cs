0461330c9432b4a14126235dbb5d1a867dd8dda8
Dmitry Shechtman <damnian@gmail.com>
2015-12-16 12:30:40 +0200

    Attempt to fix emphasis parsers

--- CommonMark/Parser/Inlines/UnderscoreParser.cs
+++ CommonMark/Parser/Inlines/UnderscoreParser.cs
@@ -16,5 +16,22 @@ namespace CommonMark.Parser.Inlines
             : base(InlineTag.Emphasis, InlineTag.Strong, parameters, settings, '_')
         {
         }
+
+        /// <summary>
+        /// Attempts to match a stack delimiter.
+        /// </summary>
+        /// <param name="subj">The source subject.</param>
+        /// <param name="startpos">The index of the first character.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <param name="beforeIsPunctuation"><c>true</c> if the substring is preceded by a punctuation character.</param>
+        /// <param name="afterIsPunctuation"><c>true</c> if the substring is followed by a punctuation character.</param>
+        /// <param name="canOpen"><c>true</c> if the delimiter can serve as an opener.</param>
+        /// <param name="canClose"><c>true</c> if the delimiter can serve as a closer.</param>
+        protected override void Match(Subject subj, int startpos, int length, bool beforeIsPunctuation, bool afterIsPunctuation, ref bool canOpen, ref bool canClose)
+        {
+            var temp = canOpen;
+            canOpen &= (!canClose || beforeIsPunctuation);
+            canClose &= (!temp || afterIsPunctuation);
+        }
     }
 }
